79117e1c3f4c4414695714bfbc951b001f053587
SIVASANKARI4 <83726490+SIVASANKARI4@users.noreply.github.com>
2021-05-21 20:03:09 +0200

    adding api to the page

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = Math.round(response.data.name);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+}
 
+let apiKey = "088d8a6c5e7273acf522248253ec0282";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Vellore&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
