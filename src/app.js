ffd4d602e34c27450d6f38e68f0334cb28ac1c98
SIVASANKARI4 <83726490+SIVASANKARI4@users.noreply.github.com>
2021-05-25 16:49:46 +0200

    Updated js file for getting realtime values

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.name;
+  let humidityElement = document.querySelector("#humidity");
+  let SpeedElement = document.querySelector("#windSpeed");
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  SpeedElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "088d8a6c5e7273acf522248253ec0282";
