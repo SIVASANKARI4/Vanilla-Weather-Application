3c4d0e65e46987b75700db91806d5deefb788a33
SIVASANKARI4 <83726490+SIVASANKARI4@users.noreply.github.com>
2021-05-25 20:52:58 +0200

    Added date element to js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+function formatDate(timestamp) {
+  //calculate date
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  return;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#city");
@@ -5,11 +14,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let SpeedElement = document.querySelector("#windSpeed");
+  let dateElement = document.querySelector("#date");
+
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   SpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "088d8a6c5e7273acf522248253ec0282";
