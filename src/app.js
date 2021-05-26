db7c9ab43e26fb6fa4aebf77eb2bb55603673abc
SIVASANKARI4 <83726490+SIVASANKARI4@users.noreply.github.com>
2021-05-26 16:21:13 +0200

    Updated the time

--- src/app.js
+++ src/app.js
@@ -2,9 +2,25 @@ function formatDate(timestamp) {
   //calculate date
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-  return;
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
