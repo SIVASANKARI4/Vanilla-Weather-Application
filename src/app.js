be7e7712e0683ac69bd267688d52c48b174cfef0
SIVASANKARI4 <83726490+SIVASANKARI4@users.noreply.github.com>
2021-05-30 20:10:16 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "088d8a6c5e7273acf522248253ec0282";
 
-let apiKey = "088d8a6c5e7273acf522248253ec0282";
-let city = "Vellore";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputelement = document.querySelector("#city-input");
+  search(cityInputelement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
